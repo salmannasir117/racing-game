113bdddbb74b47b5a3b04f8d7fff11fd4e6d9352
Sal Nasir <salnasir@Sals-MacBook-Pro.local>
2025-03-04 15:58:16 -0500

    add finished level logic

--- Assets/CheckpointCounter.cs
+++ Assets/CheckpointCounter.cs
@@ -7,6 +7,7 @@ public class CheckpointCounter : MonoBehaviour
     public GameObject[] userFacingCheckpoints;
     public static GameObject[] checkpoints;
     public static int currentCheckpoint = 0;
+    private bool finished = false; 
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +26,20 @@ public class CheckpointCounter : MonoBehaviour
         //     count++;
         //     SetCountText();
         // }
-        if (other.gameObject == CheckpointCounter.checkpoints[CheckpointCounter.currentCheckpoint]) {
-            Debug.Log("yes");
-            CheckpointCounter.currentCheckpoint = (CheckpointCounter.currentCheckpoint + 1) % CheckpointCounter.checkpoints.Length;
+
+        //if finished level, no need to check for collisions.
+        if (finished) return;
+        
+        //checkpoint system - check if collision is next checkpoint and do something if not already done the level
+        if (other.gameObject == checkpoints[currentCheckpoint] ) {
+            Debug.Log("hit correct checkpoint number " + currentCheckpoint);
+            currentCheckpoint = (currentCheckpoint + 1) % checkpoints.Length;
+            
+            //if we loop back to 0, we are finished.
+            if (currentCheckpoint == 0) {
+                Debug.Log("hit all checkpoints");
+                finished = true;
+            }
         }
     }
 }
